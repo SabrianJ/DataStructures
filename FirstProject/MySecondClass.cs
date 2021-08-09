11f6324f9d72776449eb90bdf5f1d3ff319d2ab3
SabrianJ <81123591+SabrianJ@users.noreply.github.com>
2021-08-09 18:21:05 +1200

    Test

---
+++ FirstProject/MySecondClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class MyClass
+    {
+        public int MyInt;
+        public string MyString;
+        public float myFloat;
+        public double MyDouble;
+
+        public MyClass()
+        {
+            MyInt = 0;
+            MyString = "string";
+            MyDouble = 0.1f;
+            MyDouble = 0.1;
+        }
+
+        public void MyMethod()
+        {
+            Console.WriteLine($"{MyInt}dsdsdsd");
+            Console.WriteLine("{0}dsdsdsd", MyInt);
+        }
+    }
+}
